c959718ff017c099a236464ec01eeaf3a1191f6b
Pedro <heeypedroo@gmail.com>
2023-08-07 15:32:21 -0300

    first commit

---
+++ src/main/java/com/example/ibmvacationrentalapi/IbmVacationRentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibmvacationrentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmVacationRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmVacationRentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ibmvacationrentalapi/IbmVacationRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibmvacationrentalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmVacationRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
